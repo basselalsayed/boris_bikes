8fbf211db70db7639a064b6c6b970fcd20ccea8c
Walker-TW <thomas.w.walker@live.co.uk>
2019-12-10 16:52:12 +0000

    Docking Station spec condensed

--- spec/boris_spec.rb
+++ spec/boris_spec.rb
@@ -4,5 +4,9 @@ describe DockingStation do
   describe '.release_bike' do
    it { expect(DockingStation.new()).to respond_to(&:release_bike) } # pass if obj.respond_to?(:foo
   end
+
+  describe '.working?' do
+    it { expect(DockingStation.new().release_bike.working?).to equal(true) }
+  end
 end
 
