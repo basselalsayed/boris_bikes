6d1d6b815a9d5d5534b5293acb9f81d989acae79
pacifica94 <alsayed.bassel@gmail.com>
2019-12-10 15:56:49 +0000

    added spec file for DockingStation, added empty release_bike method

--- spec/boris_spec.rb
+++ spec/boris_spec.rb
@@ -1,4 +1,8 @@
 require './lib/boris'
 
 describe DockingStation do
+  describe '.release_bike' do
+   it { expect(DockingStation.new()).to respond_to(&:release_bike) } # pass if obj.respond_to?(:foo
+  end
 end
+
